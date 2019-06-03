d1670595e516a75dec55e2f752965b8e1273d48f
RASHMI-KARNANY <41475821+RASHMI-KARNANY@users.noreply.github.com>
2019-06-04 00:10:42 +0530

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,26 @@
+import base64
+import cv2
+import zmq
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.connect('tcp://192.168.42.129:5555')
+
+camera = cv2.VideoCapture(0)  # init the camera
+
+
+while True:
+    try:
+        grabbed, frame = camera.read()  # grab the current frame
+        #print(grabbed, frame)
+        frame = cv2.resize(frame, (640, 480))  # resize the frame
+        encoded, buffer = cv2.imencode('.jpg', frame)
+        #cv2.imshow('abc', )
+        jpg_as_text = base64.b64encode(buffer)
+
+        footage_socket.send(jpg_as_text)
+
+    except KeyboardInterrupt:
+        camera.release()
+        cv2.destroyAllWindows()
+        break
